a55486fb7e5f2901ab24e1708d51990b13c9f04d
andrelince <andregslincefaria@gmail.com>
2024-02-26 00:28:00 +0000

    fix: created_at not set for user and account

--- banking-api/repositories/model/scope.go
+++ banking-api/repositories/model/scope.go
@@ -1,11 +1,19 @@
 package model
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 func (account *Account) BeforeCreate(tx *gorm.DB) (err error) {
 	account.AccountNumber = uuid.NewString()
+	account.CreatedAt = time.Now()
+	return
+}
+
+func (user *User) BeforeCreate(tx *gorm.DB) (err error) {
+	user.CreatedAt = time.Now()
 	return
 }
